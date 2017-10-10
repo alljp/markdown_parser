a2d1388b02532763a4d84299dde158cc77d4d341
alljp <allenjosesmailbox@gmail.com>
2017-10-11 01:05:06 +0530

    Add rule for quotes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 rules = [
   {regex: /(\#)(.*)/g, replacement: '<h1>$2</h1>'},
   {regex: /(\*\*|__)(.*?)\1/g, replacement: '<strong>$2</strong>'},
-  {regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>'}
+  {regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>'},
+  {regex: /\"(.*?)\"/g, replacement: '<q>$1</q>'}
 ]
 
 function render (md_string) {
@@ -11,6 +12,7 @@ function render (md_string) {
   return md_string
 }
 
-md_string = '#Header1\nText attributes _italic_, **italic**, __bold__, **bold**, ***bold-italic***'
+md_string = '#Header1\nText attributes _italic_, **italic**, __bold__, **bold**, ***bold-italic*** \n' +
+  '"Quote"'
 
 console.log(render(md_string))
