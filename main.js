15009a95686513ab85d70ae4265a7a6856e0f0fd
alljp <allenjosesmailbox@gmail.com>
2017-10-11 14:39:19 +0530

    Add function header() to handle header levels through 1 to 6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 rules = [
-  {regex: /(\#)(.*)/g, replacement: '<h1>$2</h1>'},
+  {regex: /(\#{1,6})(.*)/g, replacement: header},
   {regex: /(\*\*|__)(.*?)\1/g, replacement: '<strong>$2</strong>'},
   {regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>'},
   {regex: /\"(.*?)\"/g, replacement: '<q>$1</q>'},
@@ -7,6 +7,11 @@ rules = [
   {regex: /\!\[(.*?)\]\((.*?)\)/g, replacement: '<img src=\'$2\' alt=\'$1\'>'},
   {regex: /\n\>(.*)/g, replacement: blockquote}
 ]
+
+function header (match, item1, item2) {
+  let headerLevel = item1.length
+  return '<h' + headerLevel + '>' + item2 + '</h' + headerLevel + '>'
+}
 
 function blockquote (match, item1) {
   return `\n<blockquote>${item1}</blockquote>`
@@ -22,6 +27,6 @@ function render (md_string) {
 // md_string = '#Header1\nText attributes _italic_, **italic**, __bold__, **bold**, ***bold-italic*** \n' +
 //   '"Quote" \n A [link](http://example.com)'
 
-md_string = ' #  Title\n\nAnd *now* [a link](http://www.google.com) to **follow** and [another](http://yahoo.com/).\n\n* One\n* Two\n* Three\n\n## Subhead\n\nOne **two** three **four** five.\n\nOne __two__ three _four_ five __six__ seven _eight_.\n\n1. One\n2. Two\n3. Three\n\nMore text with `inline($code)` sample.\n\n> A block quote\n> across two lines.\nMore text...'
+md_string = ' # Title\n\nAnd *now* [a link](http://www.google.com) to **follow** and [another](http://yahoo.com/).\n\n* One\n* Two\n* Three\n\n## Subhead\n\nOne **two** three **four** five.\n\nOne __two__ three _four_ five __six__ seven _eight_.\n\n1. One\n2. Two\n3. Three\n\nMore text with `inline($code)` sample.\n\n> A block quote\n> across two lines.\nMore text...'
 
 console.log(render(md_string))
