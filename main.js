ef177a79482f5b3e26835576ed62e03c528661ca
alljp <allenjosesmailbox@gmail.com>
2017-10-12 09:06:44 +0530

    Make changes to confirm to the standards

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-rules = [
-  {regex: /(\#{1,6})(.*)/g, replacement: header},
+let rules = [
+  {regex: /(\n#{1,6})(.*)/g, replacement: header},
   {regex: /(\*\*|__)(.*?)\1/g, replacement: '<strong>$2</strong>'},
   {regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>'},
-  {regex: /\"(.*?)\"/g, replacement: '<q>$1</q>'},
+  {regex: /"(.*?)"/g, replacement: '<q>$1</q>'},
   {regex: /\n-{5,}/g, replacement: '\n<hr />'},
   {regex: /\[(.*?)\]\((.*?)\)/g, replacement: '<a href=\'$2\'>$1</a>'},
-  {regex: /\!\[(.*?)\]\((.*?)\)/g, replacement: '<img src=\'$2\' alt=\'$1\'>'},
-  {regex: /\n\>(.*)/g, replacement: blockquote},
+  {regex: /!\[(.*?)\]\((.*?)\)/g, replacement: '<img src=\'$2\' alt=\'$1\'>'},
+  {regex: /\n>(.*)/g, replacement: blockquote},
   {regex: /\n([^\n]+)\n/g, replacement: para}
 ]
 
@@ -16,9 +16,9 @@ function header (match, item1, item2) {
 }
 
 function para (match, item1) {
-  item = item1.trim()
-  if (/^<\/?(ul|ol|li|h|p|bl)/im.test(item)) {
-  	return '\n' + item1 + '\n'
+  let item = item1.trim()
+  if (/^<\/?(ul|ol|li|h|p|bl|q)/im.test(item)) {
+    return '\n' + item1 + '\n'
   }
   return '\n<p>' + item + '</p>\n'
 }
@@ -27,16 +27,16 @@ function blockquote (match, item1) {
   return `\n<blockquote>${item1}</blockquote>`
 }
 
-function render (md_string) {
+function render (mdString) {
   for (let i = 0; i < rules.length; i++) {
-    md_string = md_string.replace(rules[i].regex, rules[i].replacement)
+    mdString = mdString.replace(rules[i].regex, rules[i].replacement)
   }
-  return md_string
+  return mdString
 }
 
 // md_string = '#Header1\nText attributes _italic_, **italic**, __bold__, **bold**, ***bold-italic*** \n' +
 //   '"Quote" \n A [link](http://example.com)'
 
-md_string = ' # Title\n\nAnd *now* [a link](http://www.google.com) to **follow** and [another](http://yahoo.com/).\n\n* One\n* Two\n* Three\n\n## Subhead\n\nOne **two** three **four** five.\n\nOne __two__ three _four_ five __six__ seven _eight_.\n\n1. One\n2. Two\n3. Three\n\nMore text with `inline($code)` sample.\n\n> A block quote\n> across two lines.\nMore text...'
+let mdString = ' # Title\n\nAnd *now* [a link](http://www.google.com) to **follow** and [another](http://yahoo.com/).\n\n* One\n* Two\n* Three\n\n## Subhead\n\nOne **two** three **four** five.\n\nOne __two__ three _four_ five __six__ seven _eight_.\n\n1. One\n2. Two\n3. Three\n\nMore text with `inline($code)` sample.\n\n> A block quote\n> across two lines.\n"More text..."'
 
-console.log(render(md_string))
+console.log(render(mdString))
